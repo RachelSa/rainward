7daeb570a4baea4f490900bdd2de2277e9ae768f
Rachel Salois <rachel.salois@gmail.com>
2017-10-26 16:23:04 -0400

    commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+
+  // componentDidMount() {
+  //   fetch("https://api.darksky.net/forecast/c9b017e5dfead0a2f9b5465e3854e063/53.3498,6.2603")
+  //   .then(data => data.json())
+  //   .then(console.log)
+  // }
+
+  render() {
+    return (
+      <div className="App">
+      Rainward
+      </div>
+    );
+  }
+}
+
+export default App;
